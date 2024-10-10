be2d25a92eb67e24c2f2febe3448cee7e05acfa4
quint <quinten.kustermans@home.nl>
2024-10-10 12:42:42 +0200

    Fix compilation to work for C/C++

--- libraries/CAN-sim/VirtualCANBus.cpp
+++ libraries/CAN-sim/VirtualCANBus.cpp
@@ -1,5 +1,6 @@
 // VirtualCANBus.cpp
 #include "VirtualCANBus.hpp"
+#include <cstring>
 
 bool VirtualCANBus::addSenderEndpoint(const std::string& id) {
     if (senders.find(id) == senders.end()) {
@@ -37,10 +38,13 @@ bool VirtualCANBus::removeReceiverEndpoint(const std::string& id) {
 
 bool VirtualCANBus::sendData(const std::string& senderId, const std::string& data) {
     // Logic to send data
+    printf("%s\n", data.c_str());
     return senders.find(senderId) != senders.end();
 }
 
 bool VirtualCANBus::receiveData(const std::string& receiverId, std::string& data) {
     // Logic to receive data
+    std::string temp = "Test\n";
+    data = temp;
     return receivers.find(receiverId) != receivers.end();
 }
