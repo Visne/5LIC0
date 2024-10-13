57956ac3c7402d1dd8f649f5e9143c7be0d85e9a
Luc Boulesteix <49658051+ProbablyBoulesteix@users.noreply.github.com>
2024-10-13 21:04:25 +0200

    Added product POST functionality
    Changes:
    -Client nodes can now send SCAN post requests to server corresponding to customer scan actions
    -fixed/updated previous client request functionality
    TODO:
    - Allow nodes to subscribe to price database and get informed when changes happen
    - Implement real database out of storage rather than RAM using CFS

--- motes/coap_client/client.c
+++ motes/coap_client/client.c
@@ -6,19 +6,18 @@
 #include "coap-engine.h"
 #include "coap-blocking-api.h"
 #include "sys/node-id.h"
+#include "../coap-headers/coap-datatypes.h"
 
-/* FIXME: This server address is hard-coded for Cooja and link-local for unconnected border router. */
-#define SERVER_EP "coap://[fe80::201:1:1:1]"
-#define CLIENT_ID 1 //this will be different per node
-#define TOGGLE_INTERVAL 10
 
-PROCESS(client_process_v1a, "client process with limited product information request");
-AUTOSTART_PROCESSES(&client_process_v1a);
+
+
+PROCESS(client_process_v1b, "client process with customer scan");
+AUTOSTART_PROCESSES(&client_process_v1b);
 
 static struct etimer et;
 
-//client response handling block
-void client_chunk_handler(coap_message_t* response)
+void
+client_chunk_handler(coap_message_t* response)
 {
     const uint8_t* chunk;
 
@@ -29,48 +28,70 @@ void client_chunk_handler(coap_message_t* response)
 
     int len = coap_get_payload(response, &chunk);
 
-    printf("Product ID | Product price (EUR): %.*s", len, (char*)chunk);
-} 
-
-//datastructure containing a product id that the client can send to request product info
-typedef struct client_product_id { //a struct that the client sends product information for price/info get requests -> contains product ID for now
-    char product_id[16]; //the product data, eg EAN13 code
-    char blankbuffer[32]; //some data
-} client_product_id_t;
-static client_product_id_t product; //blank product information structure
+    printf("|%.*s", len, (char*)chunk);
+}
 
 ////start of process
 
-PROCESS_THREAD(client_process_v1a, ev, data)
+PROCESS_THREAD(client_process_v1b, ev, data)
 {
     static coap_endpoint_t server_ep; //defines server ip --> mke sure that server in cooja uses the ones defined above
     
     PROCESS_BEGIN();
     static coap_message_t request[1];      //defines blank request as pointer
     coap_endpoint_parse(SERVER_EP, strlen(SERVER_EP), &server_ep);
-    etimer_set(&et, TOGGLE_INTERVAL * CLOCK_SECOND); //dummy timmer var, will be used to send price update requests periodically
+    etimer_set(&et, TOGGLE_INTERVAL* node_id * CLOCK_SECOND); //dummy timmer var, will be used to send db requests periodically
 
     while (1) {
         PROCESS_YIELD(); //surrenders hardware control to cooja/contiki scheduler
         if (etimer_expired(&et)) { //if timer is finished, we'll send a price request
             //printf("node id %d\n", node_id);
-            
-            /* prepare request, TID is set by COAP_BLOCKING_REQUEST() */
-            coap_init_message(request, COAP_TYPE_CON, COAP_GET, 0); //prepare 
-            coap_set_header_uri_path(request, "test/query"); //we'll poll the "product" ressource (at test/hello until i can figure out why any other uri doenst work)
+            //two test cases
 
-            //writing to product info struct 
-            sprintf(product.product_id, "%d", node_id);
-            snprintf(product.blankbuffer, sizeof(product.blankbuffer), "StoredInfo");
+            if ((node_id % 2) == 0) { //if even node id, send scans 
 
-            char outputmsg[64]; //we'll define a buffer containing text corresponding to the product information request --> for now can only realistically send text through coap
-            
-            sprintf(outputmsg, "%s:%s \n", product.product_id, product.blankbuffer); //creating string containing ID:data (data currently blank)
-            coap_set_payload(request, (char*)&outputmsg, sizeof(outputmsg) - 1); //set the struct to be the payload
-            COAP_BLOCKING_REQUEST(&server_ep, request, client_chunk_handler); //send to server
+                /* prepare request, TID is set by COAP_BLOCKING_REQUEST() */
+                coap_init_message(request, COAP_TYPE_CON, COAP_POST, 0); //prepare 
+                coap_set_header_uri_path(request, "test/scan"); //we'll poll the "product" ressource (at test/hello until i can figure out why any other uri doenst work)
 
-            //printf("\n--Done--\n");
-            etimer_reset(&et);
+                scan_data_t scan; // a scan
+                //lets create a few database access commands (order 500 cigarettes)
+                sprintf(scan.customer_id, "%d", node_id*55); //arbitrary
+                sprintf(scan.product_id, "%d", node_id * 100000);  //ID for cigarettes
+                sprintf(scan.quantity, "%d", 500); // 500
+                sprintf(scan.command, "%d", 0); //ADD to inventory
+
+                char outputmsg[TX_LEN_POST]; //we'll define a buffer containing text corresponding to the product information request --> for now can only realistically send text through coap
+
+                sprintf(outputmsg, "%s%s%s%s%s%s%s", scan.customer_id, TRANSX_SEP, scan.product_id, TRANSX_SEP, scan.quantity, TRANSX_SEP, scan.command); //creating string containing ID:data (data currently blank)
+                //printf("%s\n",outputmsg);
+                coap_set_payload(request, (char*)&outputmsg, sizeof(outputmsg) - 1); //set the struct to be the payload
+                COAP_BLOCKING_REQUEST(&server_ep, request, client_chunk_handler); //send to server
+
+                //printf("\n--Done--\n");
+                etimer_reset(&et);
+
+            }
+            else {
+                /* prepare request, TID is set by COAP_BLOCKING_REQUEST() */
+                coap_init_message(request, COAP_TYPE_CON, COAP_GET, 0); //prepare 
+                coap_set_header_uri_path(request, "test/query"); //we'll poll the "product" ressource (at test/hello until i can figure out why any other uri doenst work)
+
+                req_product_data_t product;
+                //writing to product info struct 
+                sprintf(product.product_id, "%d", node_id);
+                snprintf(product.blankbuffer, sizeof(product.blankbuffer), "StoredInfo");
+
+                char outputmsg[TX_LEN_REQ]; //we'll define a buffer containing text corresponding to the product information request --> for now can only realistically send text through coap
+
+                sprintf(outputmsg, "%s:%s%s", product.product_id, product.blankbuffer, "\0"); //creating string containing ID:data (data currently blank)
+                coap_set_payload(request, (char*)&outputmsg, sizeof(outputmsg) - 1); //set the struct to be the payload
+                COAP_BLOCKING_REQUEST(&server_ep, request, client_chunk_handler); //send to server
+
+                //printf("\n--Done--\n");
+                etimer_reset(&et);
+
+            }
         }
     }
 
