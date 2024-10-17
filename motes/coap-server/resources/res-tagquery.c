10f130704ab6af3555aed1ae2bc8c5be38d49129
Visne <39844191+Visne@users.noreply.github.com>
2024-10-17 20:44:46 +0200

    Only initialize test database once, cleanup

--- motes/coap-server/resources/res-tagquery.c
+++ motes/coap-server/resources/res-tagquery.c
@@ -2,31 +2,28 @@
 #include "db.h"
 #include <stdio.h>
 
-static void res_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
+static product_t database[DB_SIZE];
+static bool initialized = false;
+
+static void res_get_handler(coap_message_t *request,
+                            coap_message_t *response,
+                            uint8_t *buffer,
+                            uint16_t preferred_size,
+                            int32_t *offset)
+{
+    if (!initialized) {
+        // Set up database
+        init_test_database(database, DB_SIZE);
+    }
+
+    // Returns the database entry for the product with this ID
+    product_t product = db_query_read(database, *request->payload);
+    coap_set_payload(response, &product, sizeof(product));
+}
 
 RESOURCE(res_tagquery,
-         "title=\"Tag query: ?len=0..\";rt=\"Text\"",
+         "title='Tag query'",
          res_get_handler,
          NULL,
          NULL,
          NULL);
-
-static void res_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
-{
-    // Set up database
-    static product_t database[DB_SIZE];
-    init_test_database(database, DB_SIZE);
-
-    ean13_t product_id = (ean13_t) request->payload;
-
-    // Returns the database entry for the product with this ID
-    product_t queried_product = db_query_read(database, product_id);
-
-    //int db_id = atoi(request_data.product_id); //for now/FIXME : turns the product ID into an index into database
-    //stores database data into char fields for later use
-    //char database_price[16], database_id[16];
-    //sprintf(database_id, "%s", database[db_id].product_id);
-    //sprintf(database_price, "%s", database[db_id].product_price);
-
-    coap_set_payload(response, &queried_product, sizeof(queried_product));
-}
