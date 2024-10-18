3b3ecbdc3fc94a831a247050fbc08d4a38eb07fd
quint <quinten.kustermans@home.nl>
2024-10-19 01:12:36 +0200

    Fun name generation to round off the day :P

--- motes/shared/db.h
+++ motes/shared/db.h
@@ -7,3 +7,4 @@ customer_tab_t* find_or_add_customer(customer_tab_t** head, customer_t customer_
 product_order_t* find_or_add_product(product_order_t** product_list, uint16_t product_id);
 void modify_product_quantity(product_order_t* product, uint16_t quantity, scan_type_t command);
 void wipe_customer(customer_tab_t **head, customer_t customer_id);
+void generate_product_name(char* dest);
